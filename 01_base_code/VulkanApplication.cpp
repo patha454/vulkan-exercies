23f7231ffb777d7c2cb73516ebfed07f3685b8ca
H Paterson <26947190+patha454@users.noreply.github.com>
2020-12-05 21:27:32 +1300

    Feature: Create GLFW window.

--- 01_base_code/VulkanApplication.cpp
+++ 01_base_code/VulkanApplication.cpp
@@ -1,18 +1,60 @@
+#include <stdexcept>
+#include "GLFW/glfw3.h"
 #include "VulkanApplication.h"
+
+GLFWwindow* VulkanApplication::setupWindow()
+{
+    GLFWwindow* window = nullptr;
+    if (glfwInit() == GLFW_FALSE)
+    {
+        throw std::runtime_error("GLFW could not be initialised.");
+    }
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window = glfwCreateWindow(
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        "Vulkan",
+        nullptr,
+        nullptr);
+    if (window == nullptr)
+    {
+        throw std::runtime_error("GLFW window could not be created.");
+    }
+    return window;
+}
+
+void VulkanApplication::cleanupWindow()
+{
+    if (this->window)
+    {
+        glfwDestroyWindow(this->window);
+    }
+    glfwTerminate();
+}
 
 void VulkanApplication::run()
 {
-    initVulkan();
+    this->initVulkan();
+    this->mainLoop();
+    this->cleanup();
 }
 
 void VulkanApplication::initVulkan()
 {
+    this->window = this->setupWindow();
+
 }
 
 void VulkanApplication::mainLoop()
 {
+    while (!glfwWindowShouldClose(this->window))
+    {
+        glfwPollEvents();
+    }
 }
 
 void VulkanApplication::cleanup()
 {
+    this->cleanupWindow();
 }
--- 01_base_code/VulkanApplication.h
+++ 01_base_code/VulkanApplication.h
@@ -1,13 +1,24 @@
 #ifndef VULKANEXERCISES_01_BASE_CODE_VULKANAPPLICATION_H
 #define VULKANEXERCISES_01_BASE_CODE_VULKANAPPLICATION_H
 
+#include "GLFW//glfw3.h"
+
 class VulkanApplication
 {
 public:
     void run();
 
 private:
+    static const uint32_t WINDOW_WIDTH = 800;
+    static const uint32_t WINDOW_HEIGHT = 600;
+    GLFWwindow* window = nullptr;
+
+
     void initVulkan();
+
+    GLFWwindow* setupWindow();
+
+    void cleanupWindow();
 
     void mainLoop();
 
